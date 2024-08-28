cf326ed8b35e54e23e04f95d315af1064232d5a4
sakthi-ulgebra <sakthi@ulgebra.com>
2024-08-28 16:32:33 +0530

    logo

--- twiliosms/__manifest__.py
+++ twiliosms/__manifest__.py
@@ -18,7 +18,7 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base','crm', 'ulgebra-odoo'],
-    'images': ['static/description/app_ui.png'],
+    'images': ['static/description/logo.png'],
     # always loaded
     'data': [
         'security/ir.model.access.csv',
